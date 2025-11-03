295c0b6aaaaf8a79e64c16d36ef3f4d15c5321c9
Prodeon <prodeon21@gmail.com>
2025-11-03 09:51:21 +0300

    add func in views.py for web-page

---
+++ src/views.py
@@ -0,0 +1,48 @@
+import datetime
+import pandas as pd
+from pandas.core.interchange.dataframe_protocol import DataFrame
+
+path = r"..\data\operations.xlsx"
+
+def create_datetime_object(date_str : str) -> datetime.datetime:
+    """Функция для преобразования строковой даты в объект даты datetime"""
+    date_obj = datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+
+    return date_obj
+
+def get_time_of_day(date_object : datetime.datetime) -> str:
+    """Функция для определения времени суток, использует часы в качестве определителя"""
+    hour = date_object.hour
+
+    if 5 <= hour <= 11:
+        return "Утро"
+    elif 12 <= hour <= 17:
+        return "День"
+    elif 18 <= hour <= 22:
+        return "Вечер"
+    else:
+        return "Ночь"
+
+
+def operations_reader(path_to_file : str) -> DataFrame:
+    """Функция для чтения excel файла и создания датафрейма"""
+    file = pd.read_excel(path_to_file)
+    return file
+
+def get_info_about_card(dataframe_with_operations : DataFrame) -> dict:
+    """Функция для преобразования данных из датафрейма в информацию о номерах карт,
+    общей сумме покупок по определенной карте и кэшбэка по определенной карте.
+    Вернет список словарей, где каждый словарь - описанные выше данные
+    """
+    grouped_by_card = dataframe_with_operations.groupby("Номер карты").agg({"Сумма операции" : "sum"}).abs()
+    grouped_by_card["Кэшбэк"] = round(grouped_by_card["Сумма операции"]) * 0.01
+    grouped_by_card.reset_index(inplace=True)
+    grouped_by_card.rename(columns={"Номер карты" : "last_digits",
+                                    "Сумма операции" : "total_spent",
+                                    "Кэшбэк" : "cashback"}, inplace=True)
+
+    return grouped_by_card.to_dict(orient="records")
+
+
+
+
