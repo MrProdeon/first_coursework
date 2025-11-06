2c47d5b56a927437df6d4bcd567bde98d00dca31
Prodeon <prodeon21@gmail.com>
2025-11-06 13:43:54 +0300

    add func investment_bank in services module

--- src/services.py
+++ src/services.py
@@ -2,6 +2,9 @@ import pandas as pd
 import json
 from utils.views_functions import operations_reader
 import datetime
+from math import ceil
+
+from typing import Any
 
 path = r"..\data\operations.xlsx"
 
@@ -27,3 +30,23 @@ def profitable_cashback(path_to_data_file : str, year : str, month : str) -> str
                      if row["Кэшбэк"] > 0}
 
     return json.dumps(resulted_dict,indent=4, ensure_ascii=False)
+
+
+df = operations_reader(path)
+df = df[["Дата операции", "Сумма операции"]]
+transactions = [{"Дата операции" : row["Дата операции"],
+                 "Сумма операции" : row["Сумма операции"]} for index,row in df.iterrows()]
+
+def investment_bank(month: str, transactions: list[dict[str, Any]], limit: int) -> float:
+    year, month = month.split("-")
+
+    df = pd.DataFrame(transactions)
+    df["Дата операции"] = pd.to_datetime(df["Дата операции"], dayfirst=True)
+    df = df[(df["Дата операции"].dt.year == int(year)) & (df["Дата операции"].dt.month == int(month))
+    & (df["Сумма операции"] < 0)]
+
+    result = round(sum([(ceil(abs(row["Сумма операции"]) / limit) * limit) - abs(row["Сумма операции"])
+              for index, row in df.iterrows()]), 2)
+
+
+    return result
