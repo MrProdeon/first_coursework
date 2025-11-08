85b40a991d21a25d07d921dffe04343ff1fa6d20
Prodeon <prodeon21@gmail.com>
2025-11-08 09:38:13 +0300

    little fixes in services

--- src/services.py
+++ src/services.py
@@ -9,30 +9,41 @@ from typing import Any
 
 path = r"..\data\operations.xlsx"
 
-# def profitable_cashback(path_to_data_file : str, year : str, month : str) -> str:
-#     """Функция для анализа выгоды, полученной от кэшбэка.
-#     Принимает путь до файла с операциями, год для анализа и месяц для анализа.
-#     Возвращает JSON, состоящий из словаря, в котором отображены все кэшбэки
-#     по категориям на выбранный год и месяц"""
-#
-#     readed_file = operations_reader(path_to_data_file)
-#
-#     readed_file["Дата операции"] = pd.to_datetime(readed_file["Дата операции"], dayfirst=True)
-#
-#     filter_file = readed_file[(readed_file["Дата операции"].dt.year == int(year))
-#     & (readed_file["Дата операции"].dt.month == int(month))]
-#
-#     grouped_by_category = (filter_file.groupby(by="Категория", as_index=False)
-#                            .agg({"Кэшбэк": "sum"})
-#                            .sort_values(by="Кэшбэк", ascending=False))
-#
-#     resulted_dict = {row["Категория"] : row["Кэшбэк"]
-#                      for index, row in grouped_by_category.iterrows()
-#                      if row["Кэшбэк"] > 0}
-#
-#     return json.dumps(resulted_dict,indent=4, ensure_ascii=False)
-#
-#
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("../logs/main_page.log", encoding="UTF-8", mode="a")
+file_formatter = logging.Formatter("%(asctime)s %(message)s %(funcName)s %(filename)s %(lineno)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+def profitable_cashback(path_to_data_file : str, year : str, month : str) -> str:
+    """Функция для анализа выгоды, полученной от кэшбэка.
+    Принимает путь до файла с операциями, год для анализа и месяц для анализа.
+    Возвращает JSON, состоящий из словаря, в котором отображены все кэшбэки
+    по категориям на выбранный год и месяц"""
+    resulted_dict = {}
+    try:
+        logger.info("Начало работы функции")
+        readed_file = operations_reader(path_to_data_file)
+
+        readed_file["Дата операции"] = pd.to_datetime(readed_file["Дата операции"], dayfirst=True)
+
+        filter_file = readed_file[(readed_file["Дата операции"].dt.year == int(year))
+        & (readed_file["Дата операции"].dt.month == int(month))]
+
+        grouped_by_category = (filter_file.groupby(by="Категория", as_index=False)
+                               .agg({"Кэшбэк": "sum"})
+                               .sort_values(by="Кэшбэк", ascending=False))
+
+        resulted_dict = {row["Категория"] : row["Кэшбэк"]
+                         for index, row in grouped_by_category.iterrows()
+                         if row["Кэшбэк"] > 0}
+    except Exception as error:
+        logger.error(f"Произошла ошибка {error}")
+        return resulter_dict
+    return json.dumps(resulted_dict,indent=4, ensure_ascii=False)
+
+
 df = operations_reader(path).fillna("Информация не указана.")
 transactions = df.to_dict(orient="records")
 
@@ -40,29 +51,41 @@ transactions = df.to_dict(orient="records")
 # transactions = [{"Дата операции" : row["Дата операции"],
 #                  "Сумма операции" : row["Сумма операции"]} for index,row in df.iterrows()]
 #
-# def investment_bank(month: str, transactions: list[dict[str, Any]], limit: int) -> float:
-#     year, month = month.split("-")
-#
-#     df = pd.DataFrame(transactions)
-#     df["Дата операции"] = pd.to_datetime(df["Дата операции"], dayfirst=True)
-#     df = df[(df["Дата операции"].dt.year == int(year)) & (df["Дата операции"].dt.month == int(month))
-#     & (df["Сумма операции"] < 0)]
-#
-#     result = round(sum([(ceil(abs(row["Сумма операции"]) / limit) * limit) - abs(row["Сумма операции"])
-#               for index, row in df.iterrows()]), 2)
-#
-#
-#     return result
+def investment_bank(month: str, transactions: list[dict[str, Any]], limit: int) -> float | str:
+    result = "Произошла ошибка во время выполнения"
+    try:
+        logger.info("Начало работы функции")
+        year, month = month.split("-")
+
+        df = pd.DataFrame(transactions)
+        df["Дата операции"] = pd.to_datetime(df["Дата операции"], dayfirst=True)
+        df = df[(df["Дата операции"].dt.year == int(year)) & (df["Дата операции"].dt.month == int(month))
+        & (df["Сумма операции"] < 0)]
+
+        result = round(sum([(ceil(abs(row["Сумма операции"]) / limit) * limit) - abs(row["Сумма операции"])
+                  for index, row in df.iterrows()]), 2)
+    except Exception as error:
+        logger.error(f"Произошла ошибка {error}")
+        return result
+
+
+    return result
 
 def simple_search(transactions : list[dict[str,Any]], string_for_search : str) -> str:
     """Функция для поиска подстроки в описании или категории. Принимает список словарей
     из транзакций и строку для поиска.
     Возвращает JSON, в котором все списки словарей, у которых в описании или категории
     имеется передаваемая строка"""
-    pattern = re.compile(rf"{string_for_search}")
-    result = [dict_ for dict_ in transactions
-              if pattern.search(dict_["Описание"])
-              or pattern.search(dict_["Категория"])]
+    result = []
+    try:
+        logger.info("Начало работы функции")
+        pattern = re.compile(rf"{string_for_search}")
+        result = [dict_ for dict_ in transactions
+                  if pattern.search(dict_["Описание"])
+                  or pattern.search(dict_["Категория"])]
+    except Exception as error:
+        logger.error(f"Произошла ошибка {error}")
+        return result
 
     return json.dumps(result, indent=4, ensure_ascii=False)
 
@@ -71,9 +94,15 @@ def phone_number_search(transactions : list[dict[str,Any]]) -> str:
     Принимает список словарей из транзакций.
     Возвращает JSON, в котором все списки словарей, у которых в описании есть номер телефона.
     """
-    pattern = re.compile(r"\+\d+ \d+ \d+-\d+-\d+")
-    result = [dict_ for dict_ in transactions
-              if pattern.search(dict_["Описание"])]
+    result = []
+    try:
+        logger.info("Начало работы функции")
+        pattern = re.compile(r"\+\d+ \d+ \d+-\d+-\d+")
+        result = [dict_ for dict_ in transactions
+                  if pattern.search(dict_["Описание"])]
+    except Exception as error:
+        logger.error(f"Произошла ошибка {error}")
+        return result
 
     return json.dumps(result, indent=4, ensure_ascii=False)
 
@@ -82,10 +111,16 @@ def remittance_search(transactions : list[dict[str,Any]]) -> str:
     Принимает список словарей из операций.
     Возвращает JSON, в котором список словарей, состоящий из тех словарей, в котором категория -
     это переводы, а в описании указано имя получателя и первая буква его фамилии с точкой."""
-    pattern = re.compile(r"^[А-Я][а-я]+ [А-Я]\.$")
-    result = [dict_ for dict_ in transactions
-              if pattern.search(dict_["Описание"])
-              and re.search("Переводы", dict_["Категория"])
-              ]
+    result = []
+    try:
+        logger.info("Начало работы функции")
+        pattern = re.compile(r"^[А-Я][а-я]+\s+[А-Я]\.$")
+        result = [dict_ for dict_ in transactions
+                  if pattern.search(dict_["Описание"])
+                  and re.search("Переводы", dict_["Категория"])
+                ]
+    except Exception as error:
+        logger.error(f"Произошла ошибка {error}")
+        return result
 
     return json.dumps(result, indent=4, ensure_ascii=False)
