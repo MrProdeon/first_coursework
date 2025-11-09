930f5b173c0200fe6f8f5dc96ae63292275c7747
Prodeon <prodeon21@gmail.com>
2025-11-09 16:03:25 +0300

    add logger in func in module services

--- src/services.py
+++ src/services.py
@@ -4,19 +4,21 @@ from utils.views_functions import operations_reader
 import datetime
 from math import ceil
 import re
+import logging
 
 from typing import Any
 
 path = r"..\data\operations.xlsx"
 
 logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler("../logs/main_page.log", encoding="UTF-8", mode="a")
-file_formatter = logging.Formatter("%(asctime)s %(message)s %(funcName)s %(filename)s %(lineno)s")
-file_handler.setFormatter(file_formatter)
-logger.addHandler(file_handler)
-logger.setLevel(logging.DEBUG)
+if not logger.handlers:
+    file_handler = logging.FileHandler("../logs/services.log", encoding="UTF-8", mode="a")
+    file_formatter = logging.Formatter("%(asctime)s %(message)s %(funcName)s %(filename)s %(lineno)s")
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+    logger.setLevel(logging.DEBUG)
 
-def profitable_cashback(path_to_data_file : str, year : str, month : str) -> str:
+def profitable_cashback(readed_file  : pd.DataFrame, year : str, month : str) -> str:
     """Функция для анализа выгоды, полученной от кэшбэка.
     Принимает путь до файла с операциями, год для анализа и месяц для анализа.
     Возвращает JSON, состоящий из словаря, в котором отображены все кэшбэки
@@ -24,7 +26,6 @@ def profitable_cashback(path_to_data_file : str, year : str, month : str) -> str
     resulted_dict = {}
     try:
         logger.info("Начало работы функции")
-        readed_file = operations_reader(path_to_data_file)
 
         readed_file["Дата операции"] = pd.to_datetime(readed_file["Дата операции"], dayfirst=True)
 
@@ -44,13 +45,6 @@ def profitable_cashback(path_to_data_file : str, year : str, month : str) -> str
     return json.dumps(resulted_dict,indent=4, ensure_ascii=False)
 
 
-df = operations_reader(path).fillna("Информация не указана.")
-transactions = df.to_dict(orient="records")
-
-# df = df[["Дата операции", "Сумма операции"]]
-# transactions = [{"Дата операции" : row["Дата операции"],
-#                  "Сумма операции" : row["Сумма операции"]} for index,row in df.iterrows()]
-#
 def investment_bank(month: str, transactions: list[dict[str, Any]], limit: int) -> float | str:
     result = "Произошла ошибка во время выполнения"
     try:
