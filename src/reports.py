12d8ae8bae511cb866e2fc4d17fc2d2950925a49
Prodeon <prodeon21@gmail.com>
2025-11-10 10:28:46 +0300

    add spending_by_workday in reports

--- src/reports.py
+++ src/reports.py
@@ -69,6 +69,39 @@ def spending_by_weekday(transactions: pd.DataFrame,
 
     return avg_amount_per_day
 
+def spending_by_workday(transactions: pd.DataFrame,
+                        date: Optional[str] = None) -> pd.DataFrame:
+    transactions["Дата операции"] = pd.to_datetime(transactions["Дата операции"], dayfirst=True)
+
+    if date is None:
+        date = pd.Timestamp.today()
+    else:
+        date = pd.to_datetime(date)
+
+    three_month_ago = date - pd.DateOffset(months=3)
+    transactions_with_date = transactions[(transactions["Дата операции"] <= date)
+                                          & (transactions["Дата операции"] >= three_month_ago)]
+
+    only_expenses = transactions_with_date[transactions_with_date["Сумма операции"] < 0].copy()
+
+    only_expenses["День недели"] = only_expenses["Дата операции"].dt.weekday
+
+    def work_or_weekend(day):
+        if day <= 4:
+            return "Рабочий"
+        else:
+            return "Выходной"
+
+    only_expenses["Рабочий или выходной"] = only_expenses["День недели"].apply(work_or_weekend)
+
+    expenses_by_workday = only_expenses.groupby(by="Рабочий или выходной", as_index=False).agg({"Сумма операции" : "mean"})
+
+    return expenses_by_workday
+
+#print(spending_by_workday(df, "2019-10-01 00:00:00"))
+
+
+
 
 
 
